11c8ed52edbfb5451b44e4324b02d8d32a02fccd
Chandaluri Vamsi Krishna <cvamsik99@gmail.com>
2024-01-20 12:14:59 +0530

    feat: add theme and finish navbar
    Signed-off-by: Chandaluri Vamsi Krishna <cvamsik99@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import NavBar from "@/components/NavBar";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +22,17 @@ export default function RootLayout({
       <body
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
-        <main className="relative flex flex-col min-h-screen">
-          <NavBar />
-          <div className="flex-grow flex-1">{children}</div>
-        </main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="relative flex flex-col min-h-screen">
+            <NavBar />
+            <div className="flex-grow flex-1">{children}</div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,29 @@
+import { Button } from "./ui/button";
+
 const NavBar = () => {
-    return <div className="flex h-14 mx-6 items-center justify-between">
-        <div className="font-bold text-3xl">NovaNex</div>
-        <div className="flex gap-8 font-medium text-xl">
-            <div className="hover:text-blue-500">Tab 1</div>
-            <div className="hover:text-blue-500">Tab 2</div>
-            <div className="hover:text-blue-500">Tab 3</div>
-        </div>
+  return (
+    <div className="flex h-20 mx-10 items-center justify-between">
+      <div className="flex gap-2">
+        <div className="font-bold text-3xl mr-10">NovaNex</div>
+        <Button variant="custom1" size="lg" className="text-xl">
+          Docs
+        </Button>
+        <Button variant="custom1" size="lg" className="text-xl">
+          Marketplace
+        </Button>
+      </div>
+      <div className="flex gap-6 font-medium text-xl">
+        <Button variant="secondary" size="lg">
+          Cart
+        </Button>
+        <Button variant="secondary" size="lg">
+          Profile
+        </Button>
+        <Button variant="secondary" size="lg">
+          Login
+        </Button>
+      </div>
     </div>
-}
+  );
+};
 export default NavBar;
