92fb172f5d70b2867311af7463d2f7788e214d2f
Chandaluri Vamsi Krishna <cvamsik99@gmail.com>
2024-01-21 16:18:20 +0530

    feat: implemented Cards, CartTable, Profile
    Signed-off-by: Chandaluri Vamsi Krishna <cvamsik99@gmail.com>

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { ProfilePage } from "@/components/ProfilePage";
+
+export default function Profile() {
+  return (
+    <MaxWidthWrapper>
+      <div className="font-semibold m-6 text-2xl">Your Profile</div>
+      <ProfilePage/>
+    </MaxWidthWrapper>
+  );
+}
